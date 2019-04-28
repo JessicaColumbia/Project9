eb09e7bee563907ed51a0bd38794f6db2324ce8e
Jiaxi Wen <jw3693@columbia.edu>
2019-04-28 18:45:58 +0000

    added the first test case for kid_safe func

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
 #test file
+from unittest import TestCase
+import main
+from collections import Counter
+class Test(TestCase):
+    def test_kid_safe(self):
+        words = Counter(["hello","world"])
+        self.assertAlmostEqual(main.kid_safe(words),1)
+        words = Counter(["hello", "sex"])
+        self.assertAlmostEqual(main.kid_safe(words), 0.9)
